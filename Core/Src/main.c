d724483b1ead79192dbcc6e51d8f0f219061e2a3
Vladislav Shcherbakov <vlad.shcherbakov@fireflyautomatix.com>
2025-05-08 17:07:30 -0600

    5v compensation

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -345,6 +345,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_10;
   sConfig.Rank = ADC_REGULAR_RANK_4;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
